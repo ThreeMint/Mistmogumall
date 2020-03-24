2bc3ace2f1368cea3a6fd432acfd04e73161407b
ThreeMint <mistsripples@163.com>
2020-03-24 21:06:35 +0800

    ti20324-1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-   
+    
   }
 }
 </script>
 
 <style>
-  @import "./assets/css/base.css";
+
 </style>
